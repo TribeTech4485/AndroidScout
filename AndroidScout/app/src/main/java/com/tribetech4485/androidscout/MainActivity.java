b1444b4ded4860e15156f071832084378ff43c0c
Michael Collins <michaelcollins247@gmail.com>
2018-01-12 12:18:04 -0500

    Swipe gestures to open the data management and bluetooth activities

--- AndroidScout/app/src/main/java/com/tribetech4485/androidscout/MainActivity.java
+++ AndroidScout/app/src/main/java/com/tribetech4485/androidscout/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +17,19 @@ import android.widget.TextView;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetector mDetector;
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 300;
 
     private Button appendDataButton;
     private Button uploadConnectButton;
+    private Button dataManageButton;
 
     private TextView messageText;
 
@@ -41,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetector(this,this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+
+
         teamNumberText = (EditText) findViewById(R.id.teamNumberText);
         teamNameText = (EditText) findViewById(R.id.teamNameText);
         teamScaleRatingText = (EditText) findViewById(R.id.teamScaleRatingText);
@@ -51,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         appendDataButton = (Button) findViewById(R.id.appendToDataButton);
         uploadConnectButton = (Button) findViewById(R.id.uploadConnectButton);
+        dataManageButton = (Button) findViewById(R.id.dataManagerButton);
 
         m = getPackageManager();
 
@@ -67,6 +90,16 @@ public class MainActivity extends AppCompatActivity {
                 saveTeamData();
             }
         });
+
+        dataManageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Data_Manager_Activity.class));
+            }
+        });
+
+
+
     }
 
     private void saveTeamData() {
@@ -121,5 +154,88 @@ public class MainActivity extends AppCompatActivity {
             setMessage(ex.getMessage(), true);
         }
         return readString;
+    }
+
+
+    // Gestures -----
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+
+        try {
+            if (Math.abs(event1.getY() - event2.getY()) > SWIPE_MAX_OFF_PATH) return false;
+            // Right to left swipe
+            if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) onLeftSwipe();
+            // Left to right swipe
+            else if (event2.getX() - event1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) onRightSwipe();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
+
+
+    // Functions for gestures
+    private void onLeftSwipe() {
+        startActivity(new Intent(getApplicationContext(), ConnectAndSendActivity.class));
+    }
+    private void onRightSwipe() {
+        startActivity(new Intent(getApplicationContext(), Data_Manager_Activity.class));
     }
 }
