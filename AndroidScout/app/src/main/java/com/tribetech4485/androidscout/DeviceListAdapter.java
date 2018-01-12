7119b1e4b5b2d5b409debd5808bba60d95864f7a
Michael Collins <michaelcollins247@gmail.com>
2018-01-11 23:08:02 -0500

    Bluetooth Update

---
+++ AndroidScout/app/src/main/java/com/tribetech4485/androidscout/DeviceListAdapter.java
@@ -0,0 +1,51 @@
+package com.tribetech4485.androidscout;
+
+/**
+ * Created by Michael on 1/11/2018.
+ */
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class DeviceListAdapter extends ArrayAdapter<BluetoothDevice> {
+
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<BluetoothDevice> mDevices;
+    private int  mViewResourceId;
+
+    public DeviceListAdapter(Context context, int tvResourceId, ArrayList<BluetoothDevice> devices){
+        super(context, tvResourceId,devices);
+        this.mDevices = devices;
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mViewResourceId = tvResourceId;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = mLayoutInflater.inflate(mViewResourceId, null);
+
+        BluetoothDevice device = mDevices.get(position);
+
+        if (device != null) {
+            TextView deviceName = (TextView) convertView.findViewById(R.id.deviceNameText);
+            TextView deviceAdress = (TextView) convertView.findViewById(R.id.deviceAddressText);
+
+            if (deviceName != null) {
+                deviceName.setText(device.getName());
+            }
+            if (deviceAdress != null) {
+                deviceAdress.setText(device.getAddress());
+            }
+        }
+
+        return convertView;
+    }
+
+}
