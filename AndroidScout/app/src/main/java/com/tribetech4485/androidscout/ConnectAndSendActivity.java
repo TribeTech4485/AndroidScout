8b134ea139cada4605e17c918d567311aeb2fa9e
Michael Collins <michaelcollins247@gmail.com>
2018-01-11 20:54:13 -0500

    Mostly Finished UI

---
+++ AndroidScout/app/src/main/java/com/tribetech4485/androidscout/ConnectAndSendActivity.java
@@ -0,0 +1,13 @@
+package com.tribetech4485.androidscout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectAndSendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_and_send);
+    }
+}
--- AndroidScout/app/src/main/java/com/tribetech4485/androidscout/MainActivity.java
+++ AndroidScout/app/src/main/java/com/tribetech4485/androidscout/MainActivity.java
@@ -1,13 +1,34 @@
 package com.tribetech4485.androidscout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button appendDataButton;
+    private Button uploadConnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        appendDataButton = (Button) findViewById(R.id.appendToDataButton);
+        uploadConnectButton = (Button) findViewById(R.id.uploadConnectButton);
+
+        uploadConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ConnectAndSendActivity.class));
+            }
+        });
     }
 }
