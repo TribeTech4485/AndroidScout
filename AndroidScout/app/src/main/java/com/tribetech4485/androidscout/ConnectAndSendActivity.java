e34795209639502807591df02d251db7488063f9
Michael <michaelcollins247@gmail.com>
2018-01-25 12:33:14 -0500

    Added ability to connect to different devices and save the name and address of that device

--- AndroidScout/app/src/main/java/com/tribetech4485/androidscout/ConnectAndSendActivity.java
+++ AndroidScout/app/src/main/java/com/tribetech4485/androidscout/ConnectAndSendActivity.java
@@ -3,6 +3,7 @@ package com.tribetech4485.androidscout;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,12 +11,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -32,9 +35,18 @@ public class ConnectAndSendActivity extends AppCompatActivity {
     BluetoothDevice mmDevice = null;
 
     private TextView logText;
+    private EditText deviceNameText;
+    private EditText deviceAddrText;
 
     boolean connected = false;
 
+
+    // Saved MAC addresses and names
+    String deviceName = "";
+    String deviceAddress = "";
+
+    String addressStorePath = "DEVMACADDRESS";
+    String nameStorePath = "DEVNAME";
 
     // Data rcv
     InputStream mmInputStream;
@@ -50,6 +62,10 @@ public class ConnectAndSendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_connect_and_send);
 
         logText = (TextView) findViewById(R.id.logText);
+        deviceNameText = (EditText) findViewById(R.id.nameText);
+        deviceAddrText = (EditText) findViewById(R.id.addressText);
+
+        updateAddrDataContent();
 
         connected = connectToDevice();
         showConnected();
@@ -69,7 +85,7 @@ public class ConnectAndSendActivity extends AppCompatActivity {
             for(BluetoothDevice device : pairedDevices)
             {
                 //(device.getName().equals("4485-M8") || device.getAddress().equals("00:1A:7D:DA:71:13"))
-                if(device.getName().equals("raspberrypi") || device.getAddress().equals("B8:27:EB:F9:A0:15")) //Note, you will need to change this to match the name and address of your device
+                if(device.getName().equals(deviceName) || device.getAddress().equals(deviceAddress)) //Note, you will need to change this to match the name and address of your device
                 {
                     setLogMessage("Found Specified Device: " + device.getAddress().toString(), false);
                     Log.e("4485-M8",device.getName());
@@ -177,22 +193,22 @@ public class ConnectAndSendActivity extends AppCompatActivity {
         logText.setText(message);
     }
 
-    private boolean checkFileExists() {
+    private boolean checkFileExists(String path) {
         try {
             File dir = getFilesDir();
-            File file = new File(dir, teamDataPath);
+            File file = new File(dir, path);
             if (file.exists()) return true;
         } catch (Exception ex) {
         }
         return false;
     }
 
-    private String readDataFile() {
-        if (!checkFileExists()) return "";
+    private String readDataFile(String path) {
+        if (!checkFileExists(path)) return "";
         FileInputStream readStream;
         String readString = "";
         try {
-            readStream = openFileInput(teamDataPath);
+            readStream = openFileInput(path);
 
             int content;
             while ((content = readStream.read()) != -1) {
@@ -205,9 +221,28 @@ public class ConnectAndSendActivity extends AppCompatActivity {
         return readString;
     }
 
+    private void saveData(String path, String content) {
+        FileOutputStream writeStream;
+        try {
+            writeStream = openFileOutput(path, Context.MODE_APPEND);
+            writeStream.write(content.getBytes());
+            writeStream.close();
+            //setMessage(readDataFile(), false);
+        } catch (Exception ex) {
+        }
+    }
+
+    private void updateAddrDataContent() {
+        if (checkFileExists(addressStorePath)) deviceAddress = readDataFile(addressStorePath);
+        else deviceAddress = "AA:AA:AA:AA:AA:AA";
+
+        if (checkFileExists(nameStorePath)) deviceName = readDataFile(nameStorePath);
+        else deviceName = "Device Name";
+    }
+
     // Send data with button press
     public void sendDataBtn(View view) {
-        String dataFileContents = readDataFile();
+        String dataFileContents = readDataFile(teamDataPath);
         if (dataFileContents == "") setLogMessage("File Contents Empty! Collect some team data, then send it.", false);
         else if (!connected) setLogMessage("Not connected to server!", true);
         else if (connected) {
@@ -223,5 +258,14 @@ public class ConnectAndSendActivity extends AppCompatActivity {
             connected = connectToDevice();
             showConnected();
         }
+    }
+
+    public void saveAddrDataBtn(View view) {
+        deviceName = deviceNameText.getText().toString();
+        deviceAddress = deviceAddrText.getText().toString();
+        saveData(nameStorePath, deviceName);
+        saveData(addressStorePath, deviceAddress);
+
+        updateAddrDataContent();
     }
 }
